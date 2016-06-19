02fe61ef247625e6ce557c68e9d3a2e4ad43ca70
Jack Szabo <Jack Szabo>
2016-06-19 19:08:50 +0100

    riddle solution

---
+++ LockerRiddle/Locker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockerRiddle
+{
+    public class Locker
+    {
+        private readonly int _number;
+
+        public Locker(int lockerNumber)
+        {
+            _number = lockerNumber;
+            IsOpen = false;
+        }
+
+        public int Number {
+            get { return _number;}
+        }
+
+        public bool IsOpen { get; private set; }
+
+        public bool IsDivisible(int number)
+        {
+            if (_number%number == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void FlipStatus()
+        {
+            IsOpen = !IsOpen;
+        }
+    }
+}
---
+++ LockerRiddle/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockerRiddle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Locker[] lockers = GetLockers().ToArray();
+            for (int i = 1; i <= 100; i++)
+            {
+                foreach (var currentLocker in lockers)
+                {
+                    if (currentLocker.IsDivisible(i))
+                    {
+                        currentLocker.FlipStatus();
+                    }
+                }
+            }
+
+            var opened = lockers.Where(l => l.IsOpen).ToList();
+            foreach (var locker in opened)
+            {
+                Console.WriteLine(locker.Number);
+            }
+            Console.ReadLine();
+        }
+
+        private static List<Locker> GetLockers()
+        {
+            var lockers = new List<Locker>();
+            for (int i = 1; i <= 100; i++)
+            {
+                var locker = new Locker(i);
+                lockers.Add((locker));
+            }
+            return lockers;
+        }
+    }
+}
